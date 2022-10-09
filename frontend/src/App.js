525af2bd099c320199861fb1d0508a30fe44cf51
Violetta Szewczyk <szewczykviolet@gmail.com>
2022-10-09 16:55:47 -0500

    Fetch Exercise

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Fetch Rewards Exercise</a>
+      </header>
+      <main>list payers</main>
+    </div>
+  );
+}
+
+export default App;
